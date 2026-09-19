3179941045301a30b5044dbf03938f4d218ea468
Rishabh Saraswat <rishabh_2024bcse025@gmail.com>
2026-09-19 23:39:19 +0530

    added header LICENSE

--- auth-service/src/test/java/authenticator/RegisterAuthenticatorTest.java
+++ auth-service/src/test/java/authenticator/RegisterAuthenticatorTest.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Valoq
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package authenticator;
 
 import authenticator.jwt.JwtUtil;
@@ -243,13 +266,11 @@ class RegisterAuthenticatorTest {
         @Test
         @DisplayName("Should fail with RuntimeException if database connection is prematurely closed")
         void register_ClosedConnection_ThrowsRuntimeException() throws SQLException {
-            // Open an isolated, independent connection to simulate network drop
             Connection deadConnection =
                     DriverManager.getConnection("jdbc:h2:mem:register_testdb;DB_CLOSE_DELAY=-1", "sa", "");
             UserRepository deadRepo = new UserRepository(deadConnection);
             RegisterAuthenticator deadAuthenticator = new RegisterAuthenticator(deadRepo);
 
-            // Close ONLY this local connection (leaves the test suite's connection intact)
             deadConnection.close();
 
             RuntimeException ex = Assertions.assertThrows(
